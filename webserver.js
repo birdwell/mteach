8464d79669257f8642991d617f59c4a1157f7909
Josh Birdwell <jbirdwell34@gmail.com>
2015-09-15 21:29:18 -0500

    try this now goodness

--- webserver.js
+++ webserver.js
@@ -3,12 +3,13 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname  + '/app/'));
 
 app.get('/', function(req, res) {
-  res.sendfile('index.html', {root: __dirname })
+  res.sendFile('index.html', {root: __dirname + '/app/' })
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+  console.log('This one is the dir above.' + __dirname );
 });
